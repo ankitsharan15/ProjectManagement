9f77b94845453b505359879b18061c2a0e67bbb6
Ankit Sharan <ankit@Ankits-MacBook-Pro.local>
2017-07-07 10:39:39 +0530

    code ninja started

---
+++ src/main/java/com/projectmanagement/CodeNinja/CodeNinjaApplication.java
@@ -0,0 +1,12 @@
+package com.projectmanagement.CodeNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeNinjaApplication.class, args);
+	}
+}
